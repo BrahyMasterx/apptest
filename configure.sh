93ffccca4c7599e8ea7d37cd23215ca4c6c28997
BrahyMasterx <99409937+BrahyMasterx@users.noreply.github.com>
2023-08-29 17:09:19 -0500

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
 # Run xray cloudflared ttyd
-./ttyd -c ${WEB_USERNAME}:${WEB_PASSWORD} -p 2222 -d 0 bash & 
-./cloudflared tunnel --edge-ip-version 4 --loglevel panic --protocol http2 run --token ${ARGO_AUTH} &
 ./xray -config /app/config.json
